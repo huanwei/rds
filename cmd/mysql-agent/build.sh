678ccb11096b2a436fc6f89621c0fadeaa908275
huanwei <huan@harmonycloud.cn>
2018-08-21 18:27:03 +0800

    update gitignore
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- cmd/mysql-agent/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -v -i -o ../../docker/mysql-agent/mysql-agent  main.go
--- cmd/mysql-operator/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -v -i -o ../../docker/mysql-operator/mysql-operator  main.go
